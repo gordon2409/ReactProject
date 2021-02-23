658f38856c99abbbad59c7a2e3409d982ac6c915
Luis Gordon <gordon2409@gmail.com>
2021-02-23 06:28:50 -0300

    User sign up and firebase database registration set up

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from './components/pages/homepage/homepage.component';
 import ShopPage from './components/pages/shop/shop.component'
 import SignInAndSignUpPage from './components/pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import { Route, Switch } from 'react-router-dom';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component  {
   constructor() {
@@ -18,9 +18,21 @@ class App extends React.Component  {
   unsuscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsuscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState( {currentUser: user });
-      console.log(user);
+    this.unsuscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+     if (userAuth) {
+       const userRef = await createUserProfileDocument(userAuth);
+       userRef.onSnapshot(snapShot => {
+         this.setState({
+           currentUser: {
+             id: snapShot.id,
+             ...snapShot.data()
+           }
+         })
+         console.log(this.state)
+       })
+       
+     }
+     this.setState({currentUser: userAuth});
     });
   }
 
